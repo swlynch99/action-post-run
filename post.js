efb2c870f975a1ed71083181410c5be3757e64e6
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-04-11 15:23:06 -0700

    Initial implementation

---
+++ post.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const { spawn } = require('child_process');
+const process = require('process');
+
+const run = core.getInput('run', { required: true });
+const shell = core.getInput('shell');
+const wd = core.getInput('working-directory');
+
+const subprocess = spawn(
+    run,
+    {
+        cwd: wd == '' ? undefined : wd,
+        shell: shell == '' ? true : shell,
+    }
+);
+
+subprocess.on("exit", (exitCode) => {
+    process.exitCode = exitCode;
+});
